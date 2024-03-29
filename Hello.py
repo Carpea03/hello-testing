3dfd131ba6e3844428acdc11dadd8e8087b3aeef
Carpea03 <128560891+Carpea03@users.noreply.github.com>
2024-03-29 01:24:43 +0000

    dsfasef

--- Hello.py
+++ Hello.py
@@ -14,31 +14,31 @@ st.set_page_config(page_title="Baxter Internal Tools", page_icon=":guardsman:", 
 redirect_uri = os.environ.get("REDIRECT_URI", "https://hellotesting-y175lslw65h.streamlit.app/")
 
 def auth_flow():
+    st.write("Welcome to LTC Exam Report!")
     auth_code = st.query_params.get("code")
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         "patent_examination_tool.json",
         scopes=["https://www.googleapis.com/auth/userinfo.email", "openid"],
         redirect_uri=redirect_uri,
     )
-    
     if auth_code:
         flow.fetch_token(code=auth_code)
         credentials = flow.credentials
-        
+        st.write("Login Done")
         user_info_service = build(
             serviceName="oauth2",
             version="v2",
             credentials=credentials,
         )
         user_info = user_info_service.userinfo().get().execute()
-        
+        assert user_info.get("email"), "Email not found in infos"
         st.session_state["google_auth_code"] = auth_code
         st.session_state["user_info"] = user_info
     else:
         authorization_url, state = flow.authorization_url()
         button_html = f'<a href="{authorization_url}" target="_self"><button style="background-color: #4285F4; color: white; padding: 10px 20px; border: none; border-radius: 4px; font-size: 16px; cursor: pointer;">Sign in with Google</button></a>'
         st.markdown(button_html, unsafe_allow_html=True)
-        st.empty()
+        
 def extract_info(text):
     with st.container():
          st.subheader("Text Extraction")
