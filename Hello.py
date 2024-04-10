828d342531ed99bb223cc6aa04413bf1cddb26db
Carpea03 <alexcarpenter2000@gmail.com>
2024-04-10 09:04:46 +0800

    refining

--- Hello.py
+++ Hello.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 def main():
     st.set_page_config(page_title="Patent App", page_icon=":guardsman:", layout="wide")
+    st.title("Welcome to the Baxter IP Internal Tools")
     st.write("Select an option from the sidebar to get started.")
 
 if __name__ == "__main__":
